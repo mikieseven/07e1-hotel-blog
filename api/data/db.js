afb0d8f3098089e65fb395b86ab40c31fc42830f
mikieseven <miguel@xopolus.com>
2017-02-15 15:00:39 -0600

    Use DB socket to connect

--- api/data/db.js
+++ api/data/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var dburl = 'mongodb://127.0.0.1:27017/meanhotel';
+var dburl = 'mongodb:///opt/bitnami/mongod/tmp/mongodb-27017.sock/meanhotel';
+//var dburl = 'mongodb://127.0.0.1:27017/meanhotel';
 //var dburl = 'mongodb://localhost:27017/meanhotel';
 //var dburl = 'mongodb://root:k6i2vLcngVyu@127.0.0.1:27017/meanhotel';
 //https://docs.mongodb.com/manual/reference/connection-string/
